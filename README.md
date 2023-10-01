b30fe0553261783cbbb18f528ffcc579c678753f
SnubStumpy2007 <joshua192007@gmail.com>
2023-10-01 00:09:00 -0400

    final update

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# OpenWeather
+# OpenWeather# Weather App
+
+## Description
+
+This is a simple weather app that allows users to search for the weather forecast for a specific city. It provides a 5-day weather forecast, including temperature, humidity, wind speed, and a weather description.
+
+## Features
+
+- Input field to enter the city name.
+- Search button to fetch and display the weather forecast.
+- Display of the 5-day weather forecast with date, temperature, humidity, wind speed, and weather description.
+- Error message display for invalid city names or network errors.
+
+## Technologies Used
+
+- HTML: For structuring the web page.
+- CSS: For styling the user interface.
+- JavaScript: For fetching and displaying weather data.
+- OpenWeatherMap API: For retrieving weather forecast data.
+
+## Installation
+
+1. Clone this repository to your local machine.
+
+git clone https://github.com/your-username/weather-app.git
+
+
+2. Open the `index.html` file in a web browser to use the app.
+
+## Usage
+
+1. Enter the name of the city you want to check the weather for in the search input field.
+
+2. Click the search button to fetch and display the 5-day weather forecast for the specified city.
+
+3. The weather forecast will include date, temperature, humidity, wind speed, and a weather description for each day.
+
+4. If you enter an invalid city name or encounter any errors during the process, an error message will be displayed.
+
+## Credits
+
+- This app was created by Nick Stypinski.
+- Weather data is provided by [OpenWeatherMap API](https://openweathermap.org/).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
 
 # Sources
 https://www.youtube.com/watch?v=MIYQR-Ybrn4
@@ -7,3 +53,4 @@ freepik
 bqlqn
 exomoon design studio
 smashiconsx
+TA Samuel Cordova
--- script.js
+++ script.js
@@ -1,78 +1,117 @@
+// API key for OpenWeatherMap
 const apiKey = "f413901c1e4d8123585a8c42e90ae181";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-const searchInput = document.querySelector('.search');
-const forecastContainer = document.querySelector('.forecast');
-const errorContainer = document.querySelector('.error');
-const searchForm = document.getElementById('search-form');
 
-// Function to fetch the five-day forecast
-async function fetchFiveDayForecast(city) {
-    try {
-        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        if (!response.ok) {
-            throw new Error('Unable to fetch data');
-        }
-        const data = await response.json();
+// URL for the OpenWeatherMap API
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?&units=metric&q=";
 
-        // Clear previous forecast data
-        forecastContainer.innerHTML = '';
+// DOM elements
+const searchInput = document.querySelector('#search'); // Input field
+const forecastContainer = document.querySelector('.forecast'); // Container for forecast data
+const errorContainer = document.querySelector('.error'); // Container for error messages
+const searchForm = document.getElementById('search-form'); // Search form element
 
-        // Loop through the data and populate the forecastContainer
-        for (let i = 0; i < 5; i++) { // Assuming you want to display the next 5 days
-            const dayData = data.daily[i];
-            
-            const dayElement = document.createElement('div');
-            dayElement.classList.add('day');
+// Event listener for the form submission
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const city = searchInput.value.trim(); // Get the city name from the input field
 
-            const dateElement = document.createElement('h2');
-            dateElement.classList.add('date');
-            dateElement.textContent = formatDate(dayData.date); // Implement a function to format the date
-
-            const iconElement = document.createElement('img');
-            iconElement.src = getWeatherIconUrl(dayData.icon); // Implement a function to get the weather icon URL
-            iconElement.alt = dayData.description;
-            iconElement.classList.add('weather-icon');
-
-            const tempElement = document.createElement('p');
-            tempElement.classList.add('temp');
-            tempElement.textContent = `${dayData.temp}°C`;
-
-            const descriptionElement = document.createElement('p');
-            descriptionElement.classList.add('description');
-            descriptionElement.textContent = dayData.description;
-
-            dayElement.appendChild(dateElement);
-            dayElement.appendChild(iconElement);
-            dayElement.appendChild(tempElement);
-            dayElement.appendChild(descriptionElement);
-
-            forecastContainer.appendChild(dayElement);
-        }
-
-        errorContainer.style.display = 'none';
-        forecastContainer.style.display = 'block';
-    } catch (error) {
-        console.error(error);
+    if (city) {
+        // If a city name is provided, fetch the five-day forecast
+        fetchFiveDayForecast(city);
+    } else {
+        // Handle a case where the input is empty
+        errorContainer.textContent = 'Enter a valid city name';
         errorContainer.style.display = 'block';
         forecastContainer.style.display = 'none';
     }
+})
+
+// Function to fetch the five-day forecast
+function fetchFiveDayForecast(city) {
+    // Clear error messages and previous forecast
+    forecastContainer.innerHTML = '';
+    errorContainer.style.display = 'none';
+
+    // Fetch data from the OpenWeatherMap API
+    fetch(apiUrl + city + `&appid=${apiKey}`)
+        .then(function (response) {
+            // Check if the response is successful; otherwise, throw an error
+            if (!response.ok) {
+                throw new Error('Unable to retrieve data from OpenWeather')
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data); // Log the API response data to the console
+
+            // Check if the response data contains a list of forecasts
+            if (data.list && data.list.length > 0) {
+                // Extract information for the next five days
+                const necessaryInformation = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]];
+                cycleInformation(necessaryInformation);
+            } else {
+                // Throw an error for an invalid format
+                throw new Error('Invalid Data Format')
+            }
+        })
+        .catch(function (error) {
+            console.error(error); // Log any errors to the console
+
+            // Handle any errors that occur during the fetch
+            errorContainer.textContent = 'An error occurred while fetching data. Please try again later.';
+            errorContainer.style.display = 'block';
+            forecastContainer.style.display = 'none';
+        });
 }
 
-// Event listener for the search button
-function searchQuery (event) {
-    event.preventDefault();
-    const city = searchInput.value;
-    console.log(city);
-    //fetchFiveDayForecast(city);
+// Function to cycle through and display forecast information
+function cycleInformation(data) {
+    // Iterate through the array and display information for each day
+    for (let i = 0; i < data.length; i++) {
+        const dayData = data[i];
+        const dayElement = document.createElement('div');
+        dayElement.classList.add('day'); // Add a CSS class for styling
+
+        // Create and format date element
+        const dateElement = document.createElement('h2');
+        dateElement.classList.add('date');
+        dateElement.textContent = formatDate(dayData.dt);
+
+        // Create and display temperature information
+        const tempElement = document.createElement('p');
+        tempElement.classList.add('temp');
+        tempElement.textContent = `Temperature: ${dayData.main.temp.toFixed(1)}°C`;
+
+        // Create and display humidity information
+        const humidityElement = document.createElement('p');
+        humidityElement.classList.add('humidity');
+        humidityElement.textContent = `Humidity: ${dayData.main.humidity}%`;
+
+        // Create and display wind information
+        const windElement = document.createElement('p');
+        windElement.classList.add('wind');
+        windElement.textContent = `Wind: ${dayData.wind.speed.toFixed(1)} km/h`;
+
+        // Create and display weather description
+        const weatherElement = document.createElement('p');
+        weatherElement.classList.add('weather');
+        weatherElement.textContent = `Weather: ${dayData.weather[0].description}`; // Access description property
+
+        // Append elements to the dayElement
+        dayElement.appendChild(dateElement);
+        dayElement.appendChild(tempElement);
+        dayElement.appendChild(humidityElement);
+        dayElement.appendChild(windElement);
+        dayElement.appendChild(weatherElement);
+
+        // Append the dayElement to the forecastContainer
+        forecastContainer.appendChild(dayElement);
+    }
 }
 
-searchForm.addEventListener('submit', searchQuery) 
-// Function to format the date (you can implement this function)
-function formatDate(date) {
-    // Implement date formatting logic here
-}
-
-// Function to get the weather icon URL (you can implement this function)
-function getWeatherIconUrl(icon) {
-    // Implement logic to map weather icons to image URLs
+// Function to format a timestamp into a readable date
+function formatDate(timestamp) {
+    const date = new Date(timestamp * 1000);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
 }
